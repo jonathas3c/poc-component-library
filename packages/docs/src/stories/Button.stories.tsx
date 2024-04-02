a73532952f7c272dd5001cf796bcd85c9a2dfc28
Jonathas Germano <jonathas.germano@hotmail.com>
2024-04-02 11:15:33 -0300

    feat: add Button component

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@jonathas-ui/react'
+import { ArrowRight } from 'phosphor-react'
+
+export default {
+  title: 'Data display/Button',
+  component: Button,
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Create new',
+  },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+    children: 'Small button',
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Próximo Passo
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+    children: 'Disabled',
+  },
+}
