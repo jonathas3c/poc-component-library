1e4ffacd241b60fad618a19a432c8f40572a3138
Jonathas Germano <jonathas.germano@hotmail.com>
2024-04-08 22:00:23 -0300

    ci: change lib configType settings

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -26,7 +26,7 @@ const config = {
     autodocs: true,
   },
   viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
+    if (!!configType) {
       config.base = '/poc-component-library/'
     }
     return config
